022d26abd60b04321f7d11cadbc861d037a0b23b
Nishant Chauhan <nnishant0308chauhan@gmail.com>
2026-03-05 14:05:22 +0530

    Updated Redis connection for Render deployment

--- redisClient.js
+++ redisClient.js
@@ -1,14 +1,16 @@
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
 
 client.on("error", (err) => {
-    console.log("Redis Error:", err);
+  console.log("Redis Error:", err);
 });
 
 async function connectRedis() {
-    await client.connect();
-    console.log("Connected to Redis");
+  await client.connect();
+  console.log("Connected to Redis");
 }
 
 module.exports = { client, connectRedis };
